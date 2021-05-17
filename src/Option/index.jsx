b50676ddbd8e46c982b8a4d34294dfea33b8344a
Šárka Princová Maroušková <sarka.marouskova@gmail.com>
2021-05-17 20:47:17 +0200

    Řešení

--- src/Option/index.jsx
+++ src/Option/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Icon from '../Icon/index';
 import './style.css';
+/*4) Nyní chceme komponentě Option přidat callback, pomocí kterého může informovat svého rodiče, že otázka byla zodpovězena. Přidejte tedy komponentě Option prop s názvem onSelected. V této prop očekáváme funkci. Zařiďte, aby komponenta Option zavolala funkci onSelected ve chvíli, kdy na tuto komponentu klikneme.*/
 
-const Option = (props) => (
-  <div className="option">
-    <Icon type={props.type} />
-    <div>{props.text}</div>
-  </div>
-);
+/*3-2) Nyní budeme potřebovat, aby nám komponenta Option skrze callback předala typ ikonky, na kterou uživatel kliknul. Komponenta Option tedy musí callbacku onSelect předat typ ikony, kterou zobrazuje. */
+
+const Option = ({ type, text, onSelected }) => {
+  const handleClick = () => {
+    console.log(type);
+    onSelected(type);
+  };
+
+  return (
+    <div onClick={handleClick} className="option">
+      <Icon type={type} />
+      <div>{text}</div>
+    </div>
+  );
+};
 
 export default Option;
--- src/Question/index.jsx
+++ src/Question/index.jsx
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuestionBody from '../QuestionBody/index';
 import Option from '../Option/index';
 import './style.css';
 
-const Question = (props) => (
-  <div className="question">
-    <QuestionBody 
-      iconType="symbolQuestion" 
-      text="Kdo jinému jámu kopá, sám do ní padá"
-    />
-    <div className="question__options">
-      <Option type="smileyStrongYes" text="Souhlasím"/>
-      <Option type="smileyYes" text="Spíše souhlasím"/>
-      <Option type="smileyNeutral" text="Nevím"/>
-      <Option type="smileyNo" text="Spíše nesouhlasím"/>
-      <Option type="smileyStrongNo" text="Nesouhlasím"/>
+/*3) Zařiďte následující: pokud je ve stavu answered hodnota false, chceme, aby componenta QuestionBody měla ikonku symbolQuestion. Pokud je stav true, chceme, aby typ ikonky byl symbolTick.*/
+
+/*5) V komponentě Question si vytvořte funkci handleSelect. Všem komponentám Option pak skrze prop onSelect tuto funkci předejte. Funkce handleSelect nechť nastaví stav answered na hodnotu true. Takto zajístíme, že kdykoliv uživatel klikne na nějakou možnost, stav se nám nastaví na true a tím se změní ikonka v těle otázky na fajfku.*/
+
+/*3-1) Stav answered uvnitř komponenty Question přejmenujte na answer. Nýní bude obsahovat řetězec s typem ikonky pro QuestionBody. Výchozí hodnota stavu bude symbolQuestion. */
+
+/*3-3) Funkce handleSelect v komponentě Question bude pak mít jeden parametr, který nazveme iconType. Jakmile se tato funkce zavolá, nastavíme náš stav answer na hodnotu získanou v tomto parametru. */
+
+const Question = (props) => {
+  const [answer, setAnswer] = useState('symbolQuestion');
+
+  const handleSelect = (iconType) => {
+    console.log(iconType);
+    setAnswer(iconType);
+  };
+
+  return (
+    <div className="question">
+      <QuestionBody iconType={answer} text={props.text} />
+      <div className="question__options">
+        <Option
+          onSelected={handleSelect}
+          type="smileyStrongYes"
+          text="Souhlasím"
+        />
+        <Option
+          onSelected={handleSelect}
+          type="smileyYes"
+          text="Spíše souhlasím"
+        />
+        <Option onSelected={handleSelect} type="smileyNeutral" text="Nevím" />
+        <Option
+          onSelected={handleSelect}
+          type="smileyNo"
+          text="Spíše nesouhlasím"
+        />
+        <Option
+          onSelected={handleSelect}
+          type="smileyStrongNo"
+          text="Nesouhlasím"
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Question;
